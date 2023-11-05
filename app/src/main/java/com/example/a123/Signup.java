e3d201b983ac21f499139f2fd180fa8dd696a4f5
XGkara <garaklidisxristos@gmail.com>
2023-11-05 21:27:50 +0200

    Changing through multiple layouts
    Signup? button and Login? button chaning through multiple layouts

--- app/src/main/java/com/example/a123/Signup.java
+++ app/src/main/java/com/example/a123/Signup.java
@@ -2,13 +2,26 @@ package com.example.a123;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Signup extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        TextView btn=findViewById(R.id.qLogIn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Signup.this,Login.class));
+            }
+        });
+
+
     }
 }
