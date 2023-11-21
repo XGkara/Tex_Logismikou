48e013d37af010495fae4df1d782cd42ff95d916
Lowzy2k <lacresia1@hotmail.com>
2023-11-21 22:22:07 +0200

    Adding_To_Database
    email and password getting added to the database and tries to read the data & tries to make places API work

--- app/src/main/java/com/example/a123/MainActivity.java
+++ app/src/main/java/com/example/a123/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.a123;
 
 import static android.content.ContentValues.TAG;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.helper.widget.MotionEffect;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -31,6 +34,11 @@ import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
 
 
+import com.directions.route.AbstractRouting;
+import com.directions.route.Routing;
+import com.directions.route.RoutingListener;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
@@ -42,8 +50,18 @@ import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.RectangularBounds;
+import com.google.android.libraries.places.api.net.FetchPlaceRequest;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -53,35 +71,58 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, NavigationView.OnNavigationItemSelectedListener  {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, NavigationView.OnNavigationItemSelectedListener {
+
+    private final static int LOCATION_REQUEST_CODE = 23;
+
+    final String placeId = "ChIJgUbEo8cfqokR5lP9_Wh_DaM";
+
+    final List<Place.Field> placeFields = Arrays.asList(Place.Field.ID, Place.Field.NAME);
+
+    // Construct a request object, passing the place ID and fields array.
+
 
     private final int FINE_PERMISSION_CODE = 1;
     private GoogleMap myMap;
     private SearchView mapSearchView;
-    Location currentLocation;
+    Location currentLocation, destinationLocation = null;
+
+    final FetchPlaceRequest request = FetchPlaceRequest.newInstance(placeId, placeFields);
+
     FusedLocationProviderClient fusedLocationProviderClient;
     FirebaseAuth auth;
     FirebaseUser user;
-
 
     private DatabaseReference reference;
     private String userID;
 
     private DrawerLayout drawerLayout;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         mapSearchView = findViewById(R.id.SearchView);
         auth = FirebaseAuth.getInstance();
         user = FirebaseAuth.getInstance().getCurrentUser();
+        User user1 = new User();
         reference = FirebaseDatabase.getInstance().getReference();
         userID = user.getUid();
         if (user == null) {
@@ -89,6 +130,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             startActivity(intent);
             finish();
         }
+
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         getLastLocation();
@@ -112,27 +154,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             navigationView.setCheckedItem(R.id.nav_home);
         }
 
-        TextView greetingTextView = findViewById(R.id.testText);
 
+        TextView greetingTextView = navigationView.findViewById(R.id.emailAddress);
 
-        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                User userProfile = snapshot.getValue(User.class);
-                if (userProfile != null) {
-                    String email = userProfile.email;
-                    // Set the email address in the TextView
-                    greetingTextView.setText(email);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Log.e("MainActivity", "Database Error: " + error.getMessage());
-                // Handle error if needed
-            }
-        });
     }
+
+
 
 
     private void getLastLocation() {
@@ -144,7 +171,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         task.addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
-                if (location != null){
+                if (location != null) {
                     currentLocation = location;
                     SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
                     mapFragment.getMapAsync(MainActivity.this);
@@ -159,19 +186,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 String location = mapSearchView.getQuery().toString();
                 List<Address> addressList = null;
 
-                if (location != null){
+                if (location != null) {
                     Geocoder geocoder = new Geocoder(MainActivity.this);
 
                     try {
-                        addressList = geocoder.getFromLocationName(location,1);
+                        addressList = geocoder.getFromLocationName(location, 1);
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
 
                     Address address = addressList.get(0);
-                    LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
                     myMap.addMarker(new MarkerOptions().position(latLng).title(location));
-                    myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+                    myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
                 }
                 return false;
             }
@@ -181,9 +208,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 return false;
             }
 
+
         });
-
-
 
 
     }
@@ -205,22 +231,23 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         LatLng serres = new LatLng(41.07670157862302, 23.554400400271827);
         myMap.addMarker(new MarkerOptions().position(serres).title("serres")
-                .icon(bitmapDescriptor(getApplicationContext(),R.drawable.pin)));
+                .icon(bitmapDescriptor(getApplicationContext(), R.drawable.pin)));
         LatLng hotel = new LatLng(41.10409809049689, 23.549068805161838);
         myMap.addMarker(new MarkerOptions().position(hotel).title("hotel")
-                .icon(bitmapDescriptor(getApplicationContext(),R.drawable.pin)));
+                .icon(bitmapDescriptor(getApplicationContext(), R.drawable.pin)));
 
 
         LatLng mouseio_serres = new LatLng(41.091226420839696, 23.54935511484131);
         myMap.addMarker(new MarkerOptions().position(mouseio_serres).title("mouseio_serres")
-                .icon(bitmapDescriptor(getApplicationContext(),R.drawable.pin)));
-        
+                .icon(bitmapDescriptor(getApplicationContext(), R.drawable.pin)));
 
 
         myMap.getUiSettings().setZoomControlsEnabled(true);
         myMap.getUiSettings().setCompassEnabled(true);
 
     }
+
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -260,5 +287,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
 
-
 }
+
+
+
--- app/src/main/java/com/example/a123/User.java
+++ app/src/main/java/com/example/a123/User.java
@@ -1,17 +1,50 @@
 package com.example.a123;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class User {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     public String email;
-    public String userUID;
     public String password;
 
-    public User(){
+
+    public User() {
+
     }
 
-    public User(String email, String userUID,String password){
+    public User(String email, String password) {
         this.email = email;
-        this.userUID = userUID;
         this.password = password;
+
+        db.collection("test")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+        }
     }
-}
+
+
+
 
