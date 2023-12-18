f50b81a46193a80cf6740b79f7fcb6a0bf34d906
Lowzy2k <lacresia1@hotmail.com>
2023-12-18 21:45:21 +0200

    Settings,UploadImage

--- app/src/main/java/com/example/a123/AboutUs.java
+++ app/src/main/java/com/example/a123/AboutUs.java
@@ -1,8 +1,10 @@
 package com.example.a123;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -14,14 +16,17 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class AboutUs extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
+public class AboutUs extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
+
+    private TextView navHeaderEmail, navHeaderPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        NavigationView navigationView = findViewById(R.id.nav_view);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -33,11 +38,26 @@ public class AboutUs extends AppCompatActivity implements NavigationView.OnNavig
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
-
         navigationView.setNavigationItemSelectedListener(this);
+
+        // Access the TextViews directly from the nav_header
+        navHeaderEmail = navigationView.getHeaderView(0).findViewById(R.id.UserEmail);
+
+
+        updateNavHeader();
     }
 
+    private void updateNavHeader() {
+        SharedPreferences prefs = getSharedPreferences("user_prefs", MODE_PRIVATE);
+        String storedEmail = prefs.getString("user_email", "");
+        String storedPassword = prefs.getString("user_password", "");
+
+        // Update TextViews in nav_header
+        if (navHeaderEmail != null && navHeaderPassword != null) {
+            navHeaderEmail.setText(storedEmail);
+            navHeaderPassword.setText(storedPassword);
+        }
+    }
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == R.id.nav_home) {
--- app/src/main/java/com/example/a123/Settings.java
+++ app/src/main/java/com/example/a123/Settings.java
@@ -1,8 +1,17 @@
 package com.example.a123;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -14,16 +23,54 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.io.IOException;
+
 public class Settings extends AppCompatActivity implements  NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
+
+    private EditText emailEditText, passwordEditText;
+    private Button saveButton;
+    private ImageView uploadImageView;
+    private SharedPreferences sharedPreferences;
+    private Uri selectedImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        NavigationView navigationView = findViewById(R.id.nav_view);
 
+        sharedPreferences = getSharedPreferences("user_prefs", MODE_PRIVATE);
 
+        // Find views by ID
+        emailEditText = findViewById(R.id.uploadEmail);
+        saveButton = findViewById(R.id.saveButton);
+        uploadImageView = findViewById(R.id.uploadImage);
+
+        // Set click listener for the saveButton
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Get entered email and password
+                String enteredEmail = emailEditText.getText().toString();
+                String enteredPassword = passwordEditText.getText().toString();
+
+                // Save the entered data in SharedPreferences
+                saveUserData(enteredEmail, enteredPassword);
+
+                // Update the nav_header with the entered email and password
+                updateNavHeader(enteredEmail, enteredPassword);
+            }
+        });
+
+        // Set click listener for the uploadImage
+        uploadImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openImagePicker();
+            }
+        });
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -35,9 +82,50 @@ public class Settings extends AppCompatActivity implements  NavigationView.OnNav
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
-
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void openImagePicker() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            selectedImageUri = data.getData();
+            try {
+                // Set the selected image to uploadImageView
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImageUri);
+                uploadImageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void saveUserData(String email, String password) {
+        // Use SharedPreferences.Editor to save user data
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("user_email", email);
+        editor.putString("user_password", password);
+        // Save the image URI as a string
+        editor.putString("user_image", selectedImageUri.toString());
+        editor.apply();
+    }
+
+    private void updateNavHeader(String email, String password) {
+        // Load TextViews and ImageView in nav_header by ID
+        TextView navHeaderEmail = findViewById(R.id.UserEmail);
+        ImageView navHeaderImage = findViewById(R.id.UserImage);
+
+        // Update TextViews and ImageView in nav_header
+        navHeaderEmail.setText(email);
+        if (selectedImageUri != null) {
+            navHeaderImage.setImageURI(selectedImageUri);
+        }
     }
 
     @Override
@@ -71,4 +159,8 @@ public class Settings extends AppCompatActivity implements  NavigationView.OnNav
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
+    public void onUploadImageClick(View view) {
+        openImagePicker();
+    }
 }
+
